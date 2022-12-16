c9eeddd5d11a8767a1bff8a6e7f792b2181dbd00
Funtikmq <115221794+Funtikmq@users.noreply.github.com>
2022-12-16 12:13:41 +0200

    Random Generator

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -39,12 +40,15 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.Teal;
             this.button1.Location = new System.Drawing.Point(386, 337);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.Size = new System.Drawing.Size(94, 67);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Psihologie";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
@@ -58,44 +62,52 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(21, 26);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(175, 176);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.Size = new System.Drawing.Size(225, 228);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.Color.PapayaWhip;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Location = new System.Drawing.Point(266, 26);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(165, 27);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(460, 20);
             this.textBox1.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(516, 337);
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.ForeColor = System.Drawing.Color.Teal;
+            this.button2.Location = new System.Drawing.Point(511, 337);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.Size = new System.Drawing.Size(94, 67);
             this.button2.TabIndex = 4;
             this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(638, 337);
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.ForeColor = System.Drawing.Color.Teal;
+            this.button3.Location = new System.Drawing.Point(632, 337);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.Size = new System.Drawing.Size(94, 67);
             this.button3.TabIndex = 5;
             this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-namespace RQM
+﻿namespace RQM
 {
     public partial class Form1 : Form
     {
@@ -6,14 +6,34 @@ namespace RQM
         {
             InitializeComponent();
 
-            textBox1.Text = "Citatul autorului";
-            label1.Text = "Autorul";
+            textBox1.Text = "Pentru generarea unui citat selecta-ți domeniul";
+            label1.Text = "";
         }
 
+       
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = $"Citat despre economie";
-            label1.Text = $"Autorul citatului depsre economie";
+
+            Random rand = new Random();
+            int counter = rand.Next(0,6);
+            string[] citat = new string[6];
+            citat[0] = "Primul Citat despre psihologie";
+            citat[1] = "Al doilea citat despre psihologie";
+            citat[2] = "Al treilea citat despre psihologie";
+            citat[3] = "Al patrulea citat despre psihologie";
+            citat[4] = "Al cincilea citat despre psihologie";
+            citat[5] = "Al șaselea citat despre psihologie";
+            string[] autor = new string[6];
+            autor[0] = "Eminescu";
+            autor[1] = "Calinescu";
+            autor[2] = "Petrescu";
+            autor[3] = "Caragiale";
+            autor[4] = "Barbu";
+            autor[5] = "Arghezi";
+            
+            textBox1.Text = citat[counter];
+            label1.Text = autor[counter];
         }
 
         private void button2_Click(object sender, EventArgs e)
